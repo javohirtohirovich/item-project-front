dc59495afdece20665224138d7718ab6d1e1ad91
“javohirtohirovich” <124333197+javohirtohirovich@users.noreply.github.com>
2024-01-17 18:31:08 +0500

    Add LogOut

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,13 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.less'
 })
 export class HeaderComponent {
+  public modalLogOutVisible: boolean = false;
 
+  public showLogOutModal(): void {
+    this.modalLogOutVisible = true;
+  }
+
+  public hideLogOutModal(): void {
+    this.modalLogOutVisible = false;
+  }
+
+  public saveLogOutChanges(): void {
+    localStorage.removeItem("access_token");
+    this.modalLogOutVisible = false;
+  }
 }
