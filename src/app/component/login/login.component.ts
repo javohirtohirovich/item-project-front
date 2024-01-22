68cdf16bd688098f973e9b126a29e5b4923ad5aa
javohirtohirovich <javoergashev30@gmail.com>
2024-01-22 12:19:04 +0500

    login.component.ts Format code

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -9,87 +9,102 @@ import { LoadingComponent } from '../loading/loading.component';
 import { ToastrService } from 'ngx-toastr';
 
 @Component({
-  selector: 'app-login',
-  standalone: true,
-  imports: [RouterModule, FormsModule, CommonModule,LoadingComponent],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.less'
+    selector: 'app-login',
+    standalone: true,
+    imports: [RouterModule, FormsModule, CommonModule, LoadingComponent],
+    templateUrl: './login.component.html',
+    styleUrl: './login.component.less'
 })
 export class LoginComponent {
-  //Inject userService
-  private userService: UserService = inject(UserService);
-  constructor(private toastr: ToastrService) {};
-  public userEmail: string = '';
-  public password: string = '';
-  public loading: boolean = false;
-  private router: Router = inject(Router);
+    //Konstruktor
+    constructor(private toastr: ToastrService) { };
 
-  public emailError: string = '';
-  public passwordError: string = '';
+    //Inject userService and Router
+    private userService: UserService = inject(UserService);
+    private router: Router = inject(Router);
 
+    //Variables for login
+    public userEmail: string = '';
+    public password: string = '';
 
-  public loginUser(): void {
-    this.resetErrors();
+    //Variable for Loading
+    public loading: boolean = false;
 
-    if (!this.validateForm()) {
-      return;
-    }
+    //Variables for Error
+    public emailError: string = '';
+    public passwordError: string = '';
 
-    const userLoginModel = new UserLogin();
-    userLoginModel.clientId = 0;
-    userLoginModel.grantType = 'password';
-    userLoginModel.password = this.password;
-    userLoginModel.userName = this.userEmail;
-    this.loading = true;
-
-    this.userService.userLogin(userLoginModel).subscribe({
-      next: (response) => {
-        this.loading = false;
-        AuthenticationOrchestrator.signaller.next(true);
-        this.router.navigate(['/home']);
-        this.toastr.success('Successful Login!');
-      },
-      error: (err) => {
-        if (err.status == 404) {
-          this.toastr.warning('No such email user found!');
-        } else {
-          this.toastr.warning('Error during login!');
+    //Function for login button
+    public loginUser(): void {
+        //Clear errorVariables
+        this.resetErrors();
+        
+        //Validate LoginForm
+        if (!this.validateForm()) {
+            return;
         }
-        this.loading = false;
-      },
-    });
-  }
 
+        //UserLoginModel to fill
+        const userLoginModel = new UserLogin();
+        userLoginModel.clientId = 0;
+        userLoginModel.grantType = 'password';
+        userLoginModel.password = this.password;
+        userLoginModel.userName = this.userEmail;
 
-  private validateForm(): boolean {
-    let isValid = true;
+        //Turn on Loadind
+        this.loading = true;
 
-    if (!this.userEmail) {
-      this.emailError = 'Email is required';
-      isValid = false;
-    } else if (!this.isValidEmail(this.userEmail)) {
-      this.emailError = 'Invalid email format';
-      isValid = false;
+        this.userService.userLogin(userLoginModel).subscribe({
+            next: (response) => {
+                //Turn off Loading
+                this.loading = false;
+
+                //Hide Login and Register Button
+                AuthenticationOrchestrator.signaller.next(true);
+
+                this.router.navigate(['/home']);
+                this.toastr.success('Successful Login!');
+            },
+            error: (err) => {
+                if (err.status == 404) {
+                    this.toastr.warning('No such email user found!');
+                } else {
+                    this.toastr.warning('Error during login!');
+                }
+                //Turn off Loading                
+                this.loading = false;
+            }
+        });
     }
 
-    if (!this.password) {
-      this.passwordError = 'Password is required';
-      isValid = false;
+    //Function Validate LoginForm
+    private validateForm(): boolean {
+        let isValid = true;
+
+        if (!this.userEmail) {
+            this.emailError = 'Email is required!';
+            isValid = false;
+        } else if (!this.isValidEmail(this.userEmail)) {
+            this.emailError = 'Invalid email format!';
+            isValid = false;
+        }
+
+        if (!this.password) {
+            this.passwordError = 'Password is required!';
+            isValid = false;
+        }
+        return isValid;
     }
-   
 
-    return isValid;
-  }
+    //Function Validate email
+    private isValidEmail(email: string): boolean {
+        const emailRegex = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
+        return emailRegex.test(email);
+    }
 
-  private isValidEmail(email: string): boolean {
-    
-    const emailRegex = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
-
-    return emailRegex.test(email);
-  }
-
-  private resetErrors(): void {
-    this.emailError = '';
-    this.passwordError = '';
-  }
+    //FUnction Clear error variables
+    private resetErrors(): void {
+        this.emailError = '';
+        this.passwordError = '';
+    }
 }
