82704615d14683564e8a56c8d65d61dc1f957044
javohirtohirovich <javoergashev30@gmail.com>
2024-01-22 13:36:41 +0500

    Register Validation Compilate

--- src/app/component/register/register.component.ts
+++ src/app/component/register/register.component.ts
@@ -30,38 +30,113 @@ export class RegisterComponent {
     public password: string = '';
     public passwordConfirm: string = '';
 
+    //Variables for Error
+    public nameError: string = '';
+    public lastNameError: string = '';
+    public emailError: string = '';
+    public passwordError: string = '';
+    public passwordErrorConfirm: string = '';
+
     //Variable for loading
     public loading: boolean = false;
 
     //Function for register
     public registerUser(): void {
+
+         //Clear errorVariables
+         this.resetErrors();
+        
+         //Validate LoginForm
+         if (!this.validateForm()) {
+             return;
+         }
+ 
         //Loading turn on
         this.loading = true;
+        //populate UserRegister with variables
+        const userRegister = new UserRegister();
+        userRegister.userName = this.userName;
+        userRegister.userLastName = this.userLastName;
+        userRegister.userEmail = this.userEmail;
+        userRegister.password = this.password;
 
-        if (this.password === this.passwordConfirm) {
+        this.userService.userRegister(userRegister).subscribe({
+            next: (response) => {
+                this.toastr.success("Successful Register!");
+                this.loading = false;
+                this.router.navigate(["/login"])
+            },
+            error: (err) => {
+                this.toastr.warning("Error during register!");
+                this.loading = false;
+            },
+        });
 
-            //populate UserRegister with variables
-            const userRegister = new UserRegister();
-            userRegister.userName = this.userName;
-            userRegister.userLastName = this.userLastName;
-            userRegister.userEmail = this.userEmail;
-            userRegister.password = this.password;
+    }
 
-            this.userService.userRegister(userRegister).subscribe({
-                next: (response) => {
-                    this.toastr.success("Successful Register!");
-                    this.loading = false;
-                    this.router.navigate(["/login"])
-                },
-                error: (err) => {
-                    this.toastr.warning("Error during register!");
-                    this.loading = false;
-                },
-            });
+    //Function Validate LoginForm
+    private validateForm(): boolean {
+        let isValid = true;
+        if (!this.userName) {
+            this.nameError = 'Name is required!';
+            isValid = false
         }
-        else {
-            this.loading = false;
-            this.toastr.warning('Passwords do not match!');
+
+        if (!this.userLastName) {
+            this.lastNameError = 'LastName is required!';
+            isValid = false
         }
+
+        if (!this.userEmail) {
+            this.emailError = 'Email is required!';
+            isValid = false;
+        } else if (!this.isValidEmail(this.userEmail)) {
+            this.emailError = 'Invalid email format!';
+            isValid = false;
+        }
+
+        if (!this.password) {
+            this.passwordError = 'Password is required!';
+            isValid = false;
+        } else if (!this.isValidPassword(this.password)) {
+            this.passwordError = 'Password is not strong!'
+        }
+
+        if (!this.passwordConfirm) {
+            this.passwordErrorConfirm = 'Password Confirm is required!';
+            isValid = false;
+        } else if (!this.isValidPasswordsMatch(this.password, this.passwordConfirm)) {
+            this.passwordErrorConfirm = 'Passwords is not Match!'
+            isValid=false;
+        }
+
+
+        return isValid;
+    }
+
+    //Function Validate email
+    private isValidEmail(email: string): boolean {
+        const emailRegex = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
+        return emailRegex.test(email);
+    }
+
+    //Function Validate password
+    private isValidPassword(password: string): boolean {
+        const passwordRegex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/;
+        return passwordRegex.test(password);
+    }
+
+    //Function Validate Passowrds match
+    private isValidPasswordsMatch(password1: string, password2: string): boolean {
+        return password1 === password2;
+    }
+
+    //FUnction Clear error variables
+    private resetErrors(): void {
+        this.nameError = '';
+        this.lastNameError = '';
+        this.passwordErrorConfirm = '';
+        this.emailError = '';
+        this.passwordError = '';
     }
 }
